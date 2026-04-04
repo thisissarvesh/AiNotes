be76359adbca5a792b1b40e706d8764a781cd2d7
thisissarvesh <thisissarveshpandey@gmail.com>
2026-04-04 13:00:54 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.post(
 );
 
 app.use(cors(
-    {origin:"http://localhost:5173",
+    {origin:"https://ainotes-5q9t.onrender.com",
         credentials:true,
          methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
     }
